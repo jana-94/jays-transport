442e6921cc9064594075a718a75b6515875adaf9
jana-94 <janaagilesoftlabs@gmail.com>
2025-04-22 18:15:03 +0530

    added loader

--- src/app/loading.jsx
+++ src/app/loading.jsx
@@ -4,12 +4,16 @@ import "../../src/app/styles/loading.css"
 
 export default function loading() {
   return (
-     <div className="loading-wrapper">
-    <div className="loading">
-      <div></div>
-      <div></div>
-      <div></div>
+  //    <div className="loading-wrapper">
+  //   <div className="loading">
+  //     <div></div>
+  //     <div></div>
+  //     <div></div>
+  //   </div>
+  // </div>
+  <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
+      <img src="/img/drive_fav_white.svg" alt="Loading..." className="spinner" />
+      <h1 style={{color: '#FFFFFF'}}>Loading...</h1>
     </div>
-  </div>
   )
 }
