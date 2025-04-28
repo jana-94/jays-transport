de183a080c9c549da53ddaedd83a15530a940133
jana-94 <janaagilesoftlabs@gmail.com>
2025-04-28 10:52:30 +0530

    fav icon update

--- src/app/layout.js
+++ src/app/layout.js
@@ -28,7 +28,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
-        <link rel="icon" type="image/svg" href="/img/fav_new_white.svg" />
+        <link rel="icon" type="image/svg" href="/img/logo_latest_white_svg.svg" />
       </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} ${montserrat.variable} antialiased`}
